577f6227c18491b2de9140e55f33a8d9e919cb93
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-10-27 23:20:34 -0700

    created search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import {MagnifyingGlass, SlidersHorizontal} from "@phosphor-icons/react";
+
+const SearchBar = () => {
+  return (
+    <div className="bg-neutral-100 w-[400px] h-[40px] rounded-3xl pl-6  pr-3 flex items-center justify-between	">
+      <MagnifyingGlass size={18} />
+      <input
+        type="text"
+        className="bg-neutral-100 text-neutral-300 text-xxs pl-2 w-3/4 "
+        placeholder="Search Food, ingredients, users, cuisines, etc"
+      ></input>
+      <div className="border border-l-[1px] border-neutral-300	h-4 ml-4"></div>
+      <SlidersHorizontal size={18} color="#A3A3A3" className="mr-2" />
+    </div>
+  );
+};
+
+export default SearchBar;
