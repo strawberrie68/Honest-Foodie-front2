0c94d058682014564afd71fc932067e8269027dc
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-10-27 22:15:27 -0700

    css files and packages

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontSize: {
+      xxs: "0.75rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
