cd9293e0af394080f9a08d6bef64eb9a99a1c8d9
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-11-01 23:52:25 -0700

    updated with constant

--- src/components/UserCategoryNav.jsx
+++ src/components/UserCategoryNav.jsx
@@ -1,40 +1,41 @@
-import {GridFour, CookingPot, NotePencil, Brain} from "@phosphor-icons/react";
+import { GridFour, CookingPot, NotePencil, Brain } from "@phosphor-icons/react";
 
 const UserCategoryNav = () => {
+  const iconSize = 20;
   return (
     <div className="flex gap-8">
-      <div className="flex  flex-col ">
-        <div className="bg-black w-full h-1 "></div>
-        <div className="flex flex-row gap-2 text-xxs font-semibold items-center mt-2 w-3/4 px-2">
+      <div className="flex flex-col ">
+        <div className="h-1 w-full bg-black "></div>
+        <div className="mt-2 flex w-3/4 flex-row items-center gap-2 px-2 text-xxs font-semibold">
           <div>
-            <GridFour size={20} />
+            <GridFour size={iconSize} />
           </div>
           <div>ALL</div>
         </div>
       </div>
-      <div className="flex  flex-col">
-        <div className="w-full h-1 "></div>
-        <div className="flex flex-row gap-2 text-xxs font-semibold items-center mt-2 w-3/4 px-2">
+      <div className="flex flex-col">
+        <div className="h-1 w-full "></div>
+        <div className="mt-2 flex w-3/4 flex-row items-center gap-2 px-2 text-xxs font-semibold">
           <div>
-            <CookingPot size={20} />
+            <CookingPot size={iconSize} />
           </div>
           <div>RECIPES</div>
         </div>
       </div>
-      <div className="flex  flex-col">
-        <div className=" w-full h-1 "></div>
-        <div className="flex flex-row gap-2 text-xxs font-semibold items-center mt-2 w-3/4 px-2">
+      <div className="flex flex-col">
+        <div className=" h-1 w-full "></div>
+        <div className="mt-2 flex w-3/4 flex-row items-center gap-2 px-2 text-xxs font-semibold">
           <div>
-            <NotePencil size={20} />
+            <NotePencil size={iconSize} />
           </div>
           <div>REVIEWS</div>
         </div>
       </div>
-      <div className="flex  flex-col">
-        <div className="w-full h-1 "></div>
-        <div className="flex flex-row gap-2 text-xxs font-semibold items-center mt-2 w-3/4 px-2">
+      <div className="flex flex-col">
+        <div className="h-1 w-full "></div>
+        <div className="mt-2 flex w-3/4 flex-row items-center gap-2 px-2 text-xxs font-semibold">
           <div>
-            <Brain size={20} />
+            <Brain size={iconSize} />
           </div>
           <div>TASTEBUDS</div>
         </div>
