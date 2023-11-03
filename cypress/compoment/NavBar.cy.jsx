e2838fa6591b3f9d03d9c4b49b6e6f366855b71a
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-11-03 10:43:16 -0700

    navbar test for inactive and active css

--- cypress/compoment/NavBar.cy.jsx
+++ cypress/compoment/NavBar.cy.jsx
@@ -2,7 +2,55 @@ import NavBar from "../../src/components/NavBar/NavBar";
 import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Render <NavBar /> component", () => {
-  it("all nav categories should be displayed", () => {
+  it("should contain categories nav text and inactive color is grey", () => {
+    cy.mount(
+      <Router>
+        <NavBar />
+      </Router>,
+    );
+    cy.get("#Home-text")
+      .should("contain", "Home")
+      .should("have.css", "color", "rgb(163, 163, 163)");
+    cy.get("#Explore-text")
+      .should("contain", "Explore")
+      .should("have.css", "color", "rgb(163, 163, 163)");
+    cy.get("#My-Recipes-text")
+      .should("contain", "My Recipes")
+      .should("have.css", "color", "rgb(163, 163, 163)");
+    cy.get("#Saved-text")
+      .should("contain", "Saved")
+      .should("have.css", "color", "rgb(163, 163, 163)");
+  });
+
+  it("categories nav text inactive color is grey", () => {
+    cy.mount(
+      <Router>
+        <NavBar />
+      </Router>,
+    );
+    cy.get("#Home-icon").should(
+      "have.css",
+      "border-color",
+      "rgb(229, 231, 235)",
+    );
+    cy.get("#Explore-icon").should(
+      "have.css",
+      "border-color",
+      "rgb(229, 231, 235)",
+    );
+    cy.get("#My-Recipes-icon").should(
+      "have.css",
+      "border-color",
+      "rgb(229, 231, 235)",
+    );
+    cy.get("#Saved-icon").should(
+      "have.css",
+      "border-color",
+      "rgb(229, 231, 235)",
+    );
+  });
+
+  it("nav categories turn black when active", () => {
     cy.mount(
       <Router>
         <NavBar />
