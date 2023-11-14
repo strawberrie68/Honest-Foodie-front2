b4394e7110c4048bff99f8845440a60967947a33
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-11-14 14:39:56 -0800

    profile header for mobile

--- cypress/component/ProfileHeader.cy.jsx
+++ cypress/component/ProfileHeader.cy.jsx
@@ -1,6 +1,9 @@
 import ProfileHeader from "../../src/components/ProfileHeader";
 import user1 from "../../src/assets/user/user1.svg";
 
+import user2 from "../../assets/user/user2.svg";
+import Pizza from "../../assets/pizza.jpg";
+
 describe("Render ProfileHeader Componenet ", () => {
   const user = {
     username: "annaReal",
@@ -9,13 +12,87 @@ describe("Render ProfileHeader Componenet ", () => {
     email: "email.com",
     picturePath: user1,
     following: ["testuser02", "testuser03"],
-    followers: ["testuser02", "testuser03", "testuser03"],
+    followers: ["testuser02", "testuser03"],
     recipes: ["testrecipe01", "testrecipe02"],
-    reviews: ["testreview01", "testreview02"],
+    reviews: [
+      {
+        recipeId: {
+          title: "Itallian Pizza",
+          picturePath: Pizza,
+          rating: 4.8,
+          tags: ["pizza", "keto"],
+          userId: {
+            username: "annieReal",
+            id: "1234",
+            firstName: "Annie",
+            lastName: "Real",
+            picturePath: user2,
+          },
+          review: [1, 2, 3, 45, 2],
+        },
+        userReview:
+          "This is a great recipe. Would make it again rtrerr rrrrrrrrtr etrrekjrth qjkertjherw ktjkewhtv ertwrewr ewrw ",
+        rating: 5,
+        timesMade: 1,
+        picturePath: Pizza,
+        isRecommend: true,
+      },
+      {
+        recipeId: {
+          title: "Sushi Pizza",
+          picturePath: Pizza,
+          rating: 4.9,
+          tags: ["pizza", "sushi"],
+          userId: {
+            username: "annieReal",
+            id: "1234",
+            firstName: "Annie",
+            lastName: "Real",
+            picturePath: user2,
+          },
+          review: [1, 2, 3, 45, 2],
+        },
+        userReview: "This is a great recipe",
+        rating: 5,
+        timesMade: 1,
+        picturePath: Pizza,
+        isRecommend: false,
+      },
+    ],
     flavorProfile: ["vegetarian", "herb-lover", "cheese"],
     caption: "vegan and vegetable lover | Food Blogger",
   };
 
+  context("mobile test", () => {
+    beforeEach(() => {
+      // run these tests as if in a phone
+      // browser with a 320 x 568 screen
+      cy.viewport("iphone-5");
+      cy.mount(<ProfileHeader user={user} />);
+    });
+    it("user image rendered", () => {
+      cy.get("img").should("have.attr", "src", user1);
+    });
+
+    it("user stats rendered #posts, #following, #follower", () => {
+      cy.get("#followings-num").contains("2");
+      cy.get("#followers-num").contains("3");
+      cy.get("#posts-num").contains("4");
+    });
+
+    it("user info rendered", () => {
+      cy.contains(user.username);
+      cy.contains(user.firstName);
+      cy.contains(user.caption);
+    });
+
+    it("flavorId tags rendered", () => {
+      cy.get("img").should("have.attr", "src", user1);
+      cy.get("#flavor-profile-tags").contains("vegetarian");
+      cy.get("#flavor-profile-tags").contains("herb-lover");
+      cy.get("#flavor-profile-tags").contains("cheese");
+    });
+  });
   context("desktop test", () => {
     beforeEach(() => {
       // run these tests as if in a desktop
@@ -63,6 +140,30 @@ describe("Render ProfileHeader Componenet when ", () => {
     caption: "vegan and vegetable lover | Food Blogger",
   };
 
+  context("mobile test", () => {
+    beforeEach(() => {
+      // run these tests as if in a desktop
+      // browser with a iphone 5
+      cy.viewport("iphone-5");
+    });
+
+    it("empty following", () => {
+      cy.mount(<ProfileHeader user={emptyUser} />);
+
+      cy.get("#followings-num").contains("0");
+    });
+
+    it("empty follower", () => {
+      cy.mount(<ProfileHeader user={emptyUser} />);
+
+      cy.get("#followers-num").contains("0");
+    });
+    it("empty recipes and reviews", () => {
+      cy.mount(<ProfileHeader user={emptyUser} />);
+
+      cy.get("#posts-num").contains("0");
+    });
+  });
   context("desktop test", () => {
     beforeEach(() => {
       // run these tests as if in a desktop
--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -88,6 +88,89 @@ const ProfileHeader = ({ user }) => {
           </div>
         </div>
       )}
+
+      {/* MOBILE MENU MODAL */}
+      {!isAboveMediumScreens && (
+        <div className="">
+          {/* top mobile navbar */}
+          <div className="flex h-14 w-full border-b px-2 py-4">
+            <div className="basis-1/5">
+              <CaretLeft size={28} color="#00000" />
+            </div>
+            <div className="flex basis-3/5 justify-center font-bold">
+              {user.username}
+            </div>
+          </div>
+          {/* username and action buttons*/}
+          <div className="mb-2 max-w-lg p-4">
+            <div className="mt-4  flex">
+              <img className="max-w-[90px]" src={User1} />
+              <div className="ml-8 w-full	">
+                <div className="flex gap-6 align-middle">
+                  <div className="text-md rounded-lg bg-black px-4 py-1 font-medium text-white">
+                    {user.username}
+                  </div>
+                  <DotsThree size={24} />
+                </div>
+                <div className="mt-6 flex h-10 items-center gap-2">
+                  <div className=" flex min-h-[20px] basis-3/4 justify-center gap-2 rounded-md bg-neutral-100 py-2 align-middle font-semibold ">
+                    Following <CaretDown size={16} color="#00000" />
+                  </div>
+                  <div className=" min-h-[20px] rounded-md bg-neutral-100 px-4 py-2">
+                    <Envelope size={20} color="#00000" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* user description */}
+            <div className="mt-4">
+              <div className="font-medium">{user.firstName}</div>
+              {user.caption}
+            </div>
+            {/* user taste id */}
+            <div className="mt-4 text-xxs">
+              {user.flavorProfile?.length > 0 && (
+                <div>
+                  <div className="font-medium text-primary-gray-500">
+                    taste id
+                  </div>
+                  <div id="flavor-profile-tags" className="mt-1 flex gap-2">
+                    {user.flavorProfile?.map((flavor, i) => (
+                      <div
+                        key={i}
+                        className="flex items-center justify-center rounded-xl border border-primary-gray-500 px-2 text-xxxs"
+                      >
+                        {flavor}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          {/* user stats*/}
+          <div className="grid h-16  grid-cols-3 justify-center border-t">
+            <div className="flex flex-col items-center justify-center">
+              <div id="posts-num" className="font-bold">
+                {totalPosts}
+              </div>
+              <div className="text-neutral-400">posts</div>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <div id="followers-num" className="font-bold">
+                {followers}
+              </div>
+              <div className="text-neutral-400">followers</div>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <div id="followings-num" className="font-bold">
+                {following}
+              </div>
+              <div className="text-neutral-400">following</div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
