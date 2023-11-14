bf9d0937f5e296e92ecd7fe2b1f37530a4e8572b
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-11-14 14:38:11 -0800

    desktop only

--- cypress/component/ProfileHeader.cy.jsx
+++ cypress/component/ProfileHeader.cy.jsx
@@ -16,36 +16,6 @@ describe("Render ProfileHeader Componenet ", () => {
     caption: "vegan and vegetable lover | Food Blogger",
   };
 
-  context("mobile test", () => {
-    beforeEach(() => {
-      // run these tests as if in a phone
-      // browser with a 320 x 568 screen
-      cy.viewport("iphone-5");
-      cy.mount(<ProfileHeader user={user} />);
-    });
-    it("user image rendered", () => {
-      cy.get("img").should("have.attr", "src", user1);
-    });
-
-    it("user stats rendered #posts, #following, #follower", () => {
-      cy.get("#followings-num").contains("2");
-      cy.get("#followers-num").contains("3");
-      cy.get("#posts-num").contains("4");
-    });
-
-    it("user info rendered", () => {
-      cy.contains(user.username);
-      cy.contains(user.firstName);
-      cy.contains(user.caption);
-    });
-
-    it("flavorId tags rendered", () => {
-      cy.get("img").should("have.attr", "src", user1);
-      cy.get("#flavor-profile-tags").contains("vegetarian");
-      cy.get("#flavor-profile-tags").contains("herb-lover");
-      cy.get("#flavor-profile-tags").contains("cheese");
-    });
-  });
   context("desktop test", () => {
     beforeEach(() => {
       // run these tests as if in a desktop
@@ -93,30 +63,6 @@ describe("Render ProfileHeader Componenet when ", () => {
     caption: "vegan and vegetable lover | Food Blogger",
   };
 
-  context("mobile test", () => {
-    beforeEach(() => {
-      // run these tests as if in a desktop
-      // browser with a iphone 5
-      cy.viewport("iphone-5");
-    });
-
-    it("empty following", () => {
-      cy.mount(<ProfileHeader user={emptyUser} />);
-
-      cy.get("#followings-num").contains("0");
-    });
-
-    it("empty follower", () => {
-      cy.mount(<ProfileHeader user={emptyUser} />);
-
-      cy.get("#followers-num").contains("0");
-    });
-    it("empty recipes and reviews", () => {
-      cy.mount(<ProfileHeader user={emptyUser} />);
-
-      cy.get("#posts-num").contains("0");
-    });
-  });
   context("desktop test", () => {
     beforeEach(() => {
       // run these tests as if in a desktop
