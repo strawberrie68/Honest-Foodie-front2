99eaea73f0ce1a5904322fac221b71ba3cbd709e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-11-28 21:01:21 -0800

    created profile - all recipe section

---
+++ src/pages/profile/ProfileSections/UserAllRecipes.jsx
@@ -0,0 +1,14 @@
+import RecipeCard from "../../../components/TypesOfRecipeCards/RecipeCard";
+
+const UserAllRecipes = ({ user, recipes }) => {
+  //Gather only users recipes
+  return (
+    <div className="recipe-card-container mt-4 grid grid-cols-3">
+      {recipes.map((recipe, i) => (
+        <RecipeCard className="recipe-Card" key={i} recipe={recipe} />
+      ))}
+    </div>
+  );
+};
+
+export default UserAllRecipes;
