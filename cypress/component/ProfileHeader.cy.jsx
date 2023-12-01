bc132f9589c1a30a90185c5a9e62f8b093c7d5cf
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-12-01 09:37:25 -0800

    updated test for mobile and desktop

--- cypress/component/ProfileHeader.cy.jsx
+++ cypress/component/ProfileHeader.cy.jsx
@@ -1,7 +1,7 @@
 import ProfileHeader from "../../src/components/ProfileHeader";
 import user1 from "../../src/assets/user/user1.svg";
 
-describe("Render ProfileHeader Componenet ", () => {
+describe("Render ProfileHeader Component ", () => {
   const user = {
     username: "annaReal",
     firstName: "Anna",
@@ -15,6 +15,36 @@ describe("Render ProfileHeader Componenet ", () => {
     flavorProfile: ["vegetarian", "herb-lover", "cheese"],
     caption: "vegan and vegetable lover | Food Blogger",
   };
+  context("mobile test", () => {
+    beforeEach(() => {
+      // run these tests as if in a phone
+      // browser with a 320 x 568 screen
+      cy.viewport("iphone-5");
+      cy.mount(<ProfileHeader user={user} />);
+    });
+    it("user image rendered", () => {
+      cy.get("img").should("have.attr", "src", user1);
+    });
+
+    it("user stats rendered #posts, #following, #follower", () => {
+      cy.get("#followings-num").contains("2");
+      cy.get("#followers-num").contains("3");
+      cy.get("#posts-num").contains("4");
+    });
+
+    it("user info rendered", () => {
+      cy.contains(user.username);
+      cy.contains(user.firstName);
+      cy.contains(user.caption);
+    });
+
+    it("flavorId tags rendered", () => {
+      cy.get("img").should("have.attr", "src", user1);
+      cy.get("#flavor-profile-tags").contains("vegetarian");
+      cy.get("#flavor-profile-tags").contains("herb-lover");
+      cy.get("#flavor-profile-tags").contains("cheese");
+    });
+  });
 
   context("desktop test", () => {
     beforeEach(() => {
@@ -48,7 +78,7 @@ describe("Render ProfileHeader Componenet ", () => {
   });
 });
 
-describe("Render ProfileHeader Componenet when ", () => {
+describe("Render ProfileHeader Component when user data is EMPTY", () => {
   const emptyUser = {
     username: "annaReal",
     firstName: "Anna",
@@ -63,6 +93,31 @@ describe("Render ProfileHeader Componenet when ", () => {
     caption: "vegan and vegetable lover | Food Blogger",
   };
 
+  context("mobile test", () => {
+    beforeEach(() => {
+      // run these tests as if in a desktop
+      // browser with a iphone 5
+      cy.viewport("iphone-5");
+    });
+
+    it("Number of following is zero", () => {
+      cy.mount(<ProfileHeader user={emptyUser} />);
+
+      cy.get("#followings-num").contains("0");
+    });
+
+    it("Number of follower is zero", () => {
+      cy.mount(<ProfileHeader user={emptyUser} />);
+
+      cy.get("#followers-num").contains("0");
+    });
+    it("Number of recipes and reviews is zero", () => {
+      cy.mount(<ProfileHeader user={emptyUser} />);
+
+      cy.get("#posts-num").contains("0");
+    });
+  });
+
   context("desktop test", () => {
     beforeEach(() => {
       // run these tests as if in a desktop
@@ -71,18 +126,18 @@ describe("Render ProfileHeader Componenet when ", () => {
       cy.mount(<ProfileHeader user={emptyUser} />);
     });
 
-    it("empty following", () => {
+    it("Number of following is zero", () => {
       cy.mount(<ProfileHeader user={emptyUser} />);
 
       cy.get("#followings-num").contains("0");
     });
 
-    it("empty follower", () => {
+    it("Number of follower is zero", () => {
       cy.mount(<ProfileHeader user={emptyUser} />);
 
       cy.get("#followers-num").contains("0");
     });
-    it("empty recipes and reviews", () => {
+    it("Number of recipes and reviews is zero", () => {
       cy.mount(<ProfileHeader user={emptyUser} />);
 
       cy.get("#posts-num").contains("0");
