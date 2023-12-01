be79f5be781fa552e4f5b978c51b6fc469653c7b
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-12-01 09:56:14 -0800

    updated

--- cypress/component/RecipeCard.cy.jsx
+++ cypress/component/RecipeCard.cy.jsx
@@ -2,7 +2,7 @@ import RecipeCard from "../../src/components/TypesOfRecipeCards/RecipeCard";
 import Pizza from "../../src/assets/pizza.jpg";
 import User1 from "../../src/assets/user/user1.svg";
 
-describe("Render RecipeCard componenet ", () => {
+describe("Render RecipeCard component ", () => {
   const recipe = {
     title: "Italian Pizza",
     description: "Authentic Italian Pizza that is to die for",
@@ -21,9 +21,10 @@ describe("Render RecipeCard componenet ", () => {
 
   it("all the user info should be rendered", () => {
     cy.mount(<RecipeCard recipe={recipe} />);
+
     cy.contains(`${recipe.userId.firstName} ${recipe.userId.lastName}`);
-    cy.contains(recipe.rating + " ⭐️");
-    cy.contains(recipe.review.length + " Reviews");
+    cy.contains(`${recipe.rating} ⭐️`);
+    cy.contains(`${recipe.review.length} Reviews`);
     cy.contains(recipe.title);
     cy.contains(recipe.description);
   });
