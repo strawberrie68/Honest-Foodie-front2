05ebe969c8a13bf1edc4fe07e489dbb29ef312a6
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-12-01 10:35:23 -0800

    updated for mobile

--- cypress/component/ProfileHeader.cy.jsx
+++ cypress/component/ProfileHeader.cy.jsx
@@ -1,11 +1,24 @@
 import ProfileHeader from "../../src/components/ProfileHeader";
 import user1 from "../../src/assets/user/user1.svg";
 
-
-import user2 from "../../assets/user/user2.svg";
-import Pizza from "../../assets/pizza.jpg";
+import user2 from "../../src/assets/user/user2.svg";
+import Pizza from "../../src/assets/pizza.jpg";
 
 describe("Render ProfileHeader Component ", () => {
+  const recipe = {
+    title: "Itallian Pizza",
+    picturePath: Pizza,
+    rating: 4.8,
+    tags: ["pizza", "keto"],
+    userId: {
+      username: "annieReal",
+      id: "1234",
+      firstName: "Annie",
+      lastName: "Real",
+      picturePath: user2,
+    },
+    review: [1, 2, 3, 45, 2],
+  };
 
   const user = {
     username: "annaReal",
@@ -14,47 +27,22 @@ describe("Render ProfileHeader Component ", () => {
     email: "email.com",
     picturePath: user1,
     following: ["testuser02", "testuser03"],
-    followers: ["testuser02", "testuser03"],
-    recipes: ["testrecipe01", "testrecipe02"],
+    followers: ["testuser02", "testuser03", "testuser03"],
+    recipes: [recipe, recipe],
     reviews: [
       {
-        recipeId: {
-          title: "Itallian Pizza",
-          picturePath: Pizza,
-          rating: 4.8,
-          tags: ["pizza", "keto"],
-          userId: {
-            username: "annieReal",
-            id: "1234",
-            firstName: "Annie",
-            lastName: "Real",
-            picturePath: user2,
-          },
-          review: [1, 2, 3, 45, 2],
-        },
+        recipeId: recipe,
         userReview:
-          "This is a great recipe. Would make it again rtrerr rrrrrrrrtr etrrekjrth qjkertjherw ktjkewhtv ertwrewr ewrw ",
+          "There's something truly magical about this pizza recipe that captivates my taste buds every time. It's not just a dish; it's a symphony of flavors that dance on my palate, creating a culinary masterpiece. ",
         rating: 5,
         timesMade: 1,
         picturePath: Pizza,
         isRecommend: true,
       },
       {
-        recipeId: {
-          title: "Sushi Pizza",
-          picturePath: Pizza,
-          rating: 4.9,
-          tags: ["pizza", "sushi"],
-          userId: {
-            username: "annieReal",
-            id: "1234",
-            firstName: "Annie",
-            lastName: "Real",
-            picturePath: user2,
-          },
-          review: [1, 2, 3, 45, 2],
-        },
-        userReview: "This is a great recipe",
+        recipeId: recipe,
+        userReview:
+          "This is a great recipe.  The crust, perfectly crisp on the outside yet delightfully chewy inside, serves as the canvas for a vibrant medley of toppings.",
         rating: 5,
         timesMade: 1,
         picturePath: Pizza,
@@ -64,36 +52,6 @@ describe("Render ProfileHeader Component ", () => {
     flavorProfile: ["vegetarian", "herb-lover", "cheese"],
     caption: "vegan and vegetable lover | Food Blogger",
   };
-  context("mobile test", () => {
-    beforeEach(() => {
-      // run these tests as if in a phone
-      // browser with a 320 x 568 screen
-      cy.viewport("iphone-5");
-      cy.mount(<ProfileHeader user={user} />);
-    });
-    it("user image rendered", () => {
-      cy.get("img").should("have.attr", "src", user1);
-    });
-
-    it("user stats rendered #posts, #following, #follower", () => {
-      cy.get("#followings-num").contains("2");
-      cy.get("#followers-num").contains("3");
-      cy.get("#posts-num").contains("4");
-    });
-
-    it("user info rendered", () => {
-      cy.contains(user.username);
-      cy.contains(user.firstName);
-      cy.contains(user.caption);
-    });
-
-    it("flavorId tags rendered", () => {
-      cy.get("img").should("have.attr", "src", user1);
-      cy.get("#flavor-profile-tags").contains("vegetarian");
-      cy.get("#flavor-profile-tags").contains("herb-lover");
-      cy.get("#flavor-profile-tags").contains("cheese");
-    });
-  });
 
   context("mobile test", () => {
     beforeEach(() => {
@@ -179,17 +137,13 @@ describe("Render ProfileHeader Component when user data is EMPTY", () => {
       cy.viewport("iphone-5");
     });
 
-
     it("Number of following is zero", () => {
-
       cy.mount(<ProfileHeader user={emptyUser} />);
 
       cy.get("#followings-num").contains("0");
     });
 
-
     it("Number of follower is zero", () => {
-
       cy.mount(<ProfileHeader user={emptyUser} />);
 
       cy.get("#followers-num").contains("0");
