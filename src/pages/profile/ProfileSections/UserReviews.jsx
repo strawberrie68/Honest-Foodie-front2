25659d98e8c6d37559e7778a4cbc75416cb2b33e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-01-20 15:58:29 -0800

    chore: update user review

--- src/pages/profile/ProfileSections/UserReviews.jsx
+++ src/pages/profile/ProfileSections/UserReviews.jsx
@@ -3,15 +3,17 @@ const UserReviews = ({ user, reviews }) => {
   //Show only user's reviews
   const isEmptyReviews = reviews.length === 0 ? true : false;
   return (
-    <div className="mt-4 grid grid-cols-3 gap-2 sm:grid-cols-4 sm:max-w-4xl m-auto">
-      {reviews && !isEmptyReviews && (
-        <>
-          {reviews.map((review, i) => (
-            <CommentCard key={i} reviewer={user} review={review} />
-          ))}
-        </>
-      )}
-      {isEmptyReviews && <div>No reviews yet</div>}
+    <div>
+      <div className="user-reviews mt-4 grid grid-cols-3 gap-2 sm:grid-cols-4 sm:max-w-4xl m-auto">
+        {reviews && !isEmptyReviews && (
+          <>
+            {reviews.map((review, i) => (
+              <CommentCard key={i} reviewer={user} review={review} />
+            ))}
+          </>
+        )}
+      </div>
+      {isEmptyReviews && <div className="empty-reviews">No reviews yet</div>}
     </div>
   );
 };
