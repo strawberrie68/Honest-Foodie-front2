40e7c8ea73218fbd6e4bb13fe881ab72b8fa027b
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-02-05 10:04:17 -0800

    refactor: UserAllRecipes Remove Comment

--- src/pages/profile/ProfileSections/UserAllRecipes.jsx
+++ src/pages/profile/ProfileSections/UserAllRecipes.jsx
@@ -1,7 +1,6 @@
 import ProfilePost from "../../../components/TypesOfRecipeCards/ProfilePost";
 
 const UserAllRecipes = ({ recipes }) => {
-  //Gather only users recipes
   return (
     <div className="recipe-card-container sm:grid">
       {recipes.map((recipe, i) => (
