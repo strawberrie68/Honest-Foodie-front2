f75885712f8edbb158bdb6e9e2120467597484c0
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-02-11 10:19:07 -0800

    user review

--- cypress/component/UserReviews.cy.jsx
+++ cypress/component/UserReviews.cy.jsx
@@ -12,16 +12,14 @@ describe("UserReviews", () => {
       );
     });
 
-    it("should render the correct number of RecipeCard components", () => {
+    it("should render the correct number of ProfilePost components", () => {
       cy.get(".user-reviews")
         .children()
         .should("have.length", testUser.reviews.length);
     });
 
     it("should not render text", () => {
-
       cy.get(".empty-reviews").should("not.exist");
-
     });
   });
 
@@ -34,14 +32,12 @@ describe("UserReviews", () => {
         </BrowserRouter>
       );
     });
-    it("should not render RecipeCard", () => {
-
+    it("should not render ProfilePost", () => {
       cy.get(".user-reviews").children().should("have.length", 0);
     });
 
     it("should render correct text", () => {
       cy.get(".empty-reviews").contains("No reviews yet");
-
     });
   });
 });
