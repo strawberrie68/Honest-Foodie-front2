c5032c106d6dde295ea060d57c43e8c6d9973b45
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-02-13 17:15:29 -0800

    refactor picturePath

--- src/shared/featuredCategories.js
+++ src/shared/featuredCategories.js
@@ -1,23 +1,18 @@
-import eggs from "../assets/featuredCategories/egg.jpg";
-import toast from "../assets/featuredCategories/toast.jpg";
-import breakfast from "../assets/featuredCategories/breakfast.jpg";
-import drink from "../assets/featuredCategories/drink.jpg";
-
 export const featuredCategories = [
   {
-    picturePath: eggs,
+    picturePath: "../assets/featuredCategories/egg.jpg",
     title: "Top Recipes",
   },
   {
-    picturePath: toast,
+    picturePath: "../assets/featuredCategories/toast.jpg",
     title: "Top Trending",
   },
   {
-    picturePath: breakfast,
+    picturePath: "../assets/featuredCategories/breakfast.jpg",
     title: "Popular",
   },
   {
-    picturePath: drink,
+    picturePath: "../assets/featuredCategories/drink.jpg",
     title: "Celebration",
   },
 ];
