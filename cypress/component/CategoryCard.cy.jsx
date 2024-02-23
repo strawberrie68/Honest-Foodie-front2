f3395ff9b1f7e5810f2d04ce6b15e7adf1dbbe49
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-02-22 20:14:45 -0800

    build: category card test created

---
+++ cypress/component/CategoryCard.cy.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import CategoryCard from "../../src/components/CategoryCard.jsx";
+import { categoriesIcon } from "../../src/shared/categoriesIcon.js";
+
+const { icon, name } = categoriesIcon[0];
+
+describe("CategoryCard", () => {
+  beforeEach(() => {
+    cy.mount(
+      <Router>
+        <CategoryCard icon={icon} name={name} />
+      </Router>
+    );
+  });
+
+  it("displays the category name", () => {
+    cy.contains(name);
+    cy.contains(icon);
+  });
+});
