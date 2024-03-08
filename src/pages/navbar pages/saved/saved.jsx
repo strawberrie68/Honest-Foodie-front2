a00fccad7dfdfdcb7eb7671b4e8d6c49850274c6
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-03-07 17:07:59 -0800

    refactor: saved page

--- src/pages/navbar pages/saved/saved.jsx
+++ src/pages/navbar pages/saved/saved.jsx
@@ -8,27 +8,29 @@ import ProfilePost from "../../../components/TypesOfRecipeCards/ProfilePost";
 const Saved = () => {
   const user = useSelector((state) => state.auth.user);
   const savedRecipes = useSelector((state) => state.auth.user?.savedRecipes);
-  console.log(savedRecipes);
-  console.log(user);
+
+  const ICON_SIZE = 18;
+  const ICON_BG_COLOR = "#A7A7A7";
+
   return (
     <div className="flex">
       <NavBar />
-      <div className="w-3/4  mt-10 flex p-4 h-full m-auto  flex-col">
+      <div className="w-3/4 mt-10 flex p-4 h-full m-auto flex-col">
         <div className="flex justify-start flex-col w-full">
           <div className="flex flex-col xs:flex-row justify-between items-center">
             <h1 className="mt-6 font-bold text-xl text-start">Saved recipes</h1>
             <div className="bg-primary-gray-100 w-[250px] h-10 rounded-3xl px-4 items-center flex justify-between mt-4">
               <div className="flex gap-4">
-                <MagnifyingGlass size={18} />
+                <MagnifyingGlass size={ICON_SIZE} />
                 <div className="text-xxs text-primary-gray-200">Search</div>
               </div>
               <div className="flex gap-2">
                 <div className="border-px border-l border-primary-gray-200 h-4 w-0"></div>
-                <SlidersHorizontal size={18} color="#A7A7A7" />
+                <SlidersHorizontal size={ICON_SIZE} color={ICON_BG_COLOR} />
               </div>
             </div>
           </div>
-          <div className=" flex justify-start mt-8 gap-1 overflow-x-scroll  ">
+          <div className=" flex justify-start mt-8 gap-1 overflow-x-scroll">
             {categoriesIcon.map((category, i) => (
               <CategoryCard key={i} icon={category.icon} name={category.name} />
             ))}
