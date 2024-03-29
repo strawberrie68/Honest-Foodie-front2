6f79011a8be108074fbc15a9214630f506078c0e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-03-29 10:39:57 -0700

    update: navbar added responsive design for mobile

--- cypress/component/NavBar.cy.jsx
+++ cypress/component/NavBar.cy.jsx
@@ -3,7 +3,7 @@ import NavBar from "../../src/components/NavBar/NavBar.jsx";
 
 const navItems = ["Home", "Explore", "My Recipes", "Saved"];
 
-describe("CategoryCard", () => {
+describe("renders NavBar - display basic icons", () => {
   beforeEach(() => {
     cy.mount(
       <Router>
@@ -24,5 +24,43 @@ describe("CategoryCard", () => {
         .contains(navItem)
         .should("not.have.class", "text-neutral-400");
     });
+  });
+});
+
+describe("NavBar - responsive", () => {
+  beforeEach(() => {
+    cy.mount(
+      <Router>
+        <NavBar />
+      </Router>
+    );
+  });
+  it("renders correctly in mobile view", () => {
+    const width = 360;
+    cy.viewport(width, 640);
+
+    cy.get(".navbar-container").should("have.css", "height", "64px");
+    cy.get(".navbar-container").should("have.css", "width", width + "px");
+    cy.get(".navbar-container .logo").should("not.be.visible");
+    cy.get(".navbar-container .nav-link").should(
+      "have.css",
+      "flex-direction",
+      "row"
+    );
+  });
+
+  it("renders correctly in desktop view", () => {
+    const height = 768;
+    const width = 1024;
+    cy.viewport(1024, 768);
+
+    cy.get(".navbar-container").should("have.css", "height", height + "px");
+    cy.get(".navbar-container").should("not.have.css", "width", width + "px");
+    cy.get(".navbar-container .logo").should("be.visible");
+    cy.get(".navbar-container .nav-link").should(
+      "have.css",
+      "flex-direction",
+      "column"
+    );
   });
 });
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,14 +4,18 @@ import { navigationIcons } from "./NavIconData";
 
 const NavBar = () => {
   return (
-    <div className="h-full w-24 border bl-black-200 pt-10 icons-center flex flex-col">
-      <img src={logo} alt="logo" className="h-20 w-auto p-5" />
+    <div className="navbar-container h-16 xs:h-screen sticky xs:top-0 w-full  xs:w-20 border bl-black-200 xs:pt-10 icons-center flex xs:flex-col px-5 xs:px-0 bg-white align-middle items-center">
+      <img
+        src={logo}
+        alt="logo"
+        className="logo w-10 h-10 xs:h-20 xs:w-20 xs:p-5 hidden xs:block"
+      />
 
-      <div className="nav-link mt-16 p-2">
+      <div className="flex justify-between xs:flex-col nav-link xs:mt-16 p-2 w-full xs:w-20">
         {navigationIcons.map((icon) => (
           <NavLink key={icon.name} to={icon.href}>
             {({ isActive }) => (
-              <div className="flex flex-col items-center	mb-6">
+              <div className="flex flex-col items-center	xs:mb-6 ">
                 {isActive ? icon.activeIcon : icon.inactiveIcon}
                 <p
                   className={`nav-text text-xxs pt-1 text-center ${
