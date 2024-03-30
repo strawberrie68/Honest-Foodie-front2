e32c011ee2aa824e42be6485846ff8020bd67ef5
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-03-30 11:37:29 -0700

    chore: refactor and cleaned icons

--- src/components/NavBar/NavIconData.jsx
+++ src/components/NavBar/NavIconData.jsx
@@ -1,40 +1,36 @@
-import {
-  House,
-  MagnifyingGlass,
-  Binoculars,
-  Book,
-  Archive,
-} from "@phosphor-icons/react";
+import { House, Binoculars, Book, Archive } from "@phosphor-icons/react";
+
+const iconSize = 24;
+const active = "#000000";
+const inactive = "#9E9E9E";
 
 export const navigationIcons = [
   {
     name: "Home",
-    activeIcon: <House size={24} color="#000000" weight="fill" />,
-    inactiveIcon: <House size={24} color="#9E9E9E" />,
-    href: "/",
+    activeIcon: <House size={iconSize} color={active} weight="fill" />,
+    inactiveIcon: <House size={iconSize} color={inactive} />,
+    href: "/home",
+    id: "nav-icon-home",
   },
-  // {
-  //   name: "Search",
-  //   activeIcon: <MagnifyingGlass size={24} color="#000000" weight="fill" />,
-  //   inactiveIcon: <MagnifyingGlass size={24} color="#9E9E9E" />,
-  //   href: "/search",
-  // },
   {
     name: "Explore",
-    activeIcon: <Binoculars size={24} color="#000000" weight="fill" />,
-    inactiveIcon: <Binoculars size={24} color="#9E9E9E" />,
+    activeIcon: <Binoculars size={iconSize} color={active} weight="fill" />,
+    inactiveIcon: <Binoculars size={iconSize} color={inactive} />,
     href: "/explore",
+    id: "nav-icon-explore",
   },
   {
     name: "My Recipes",
-    activeIcon: <Book size={24} color="#000000" weight="fill" />,
-    inactiveIcon: <Book size={24} color="#9E9E9E" />,
+    activeIcon: <Book size={iconSize} color={active} weight="fill" />,
+    inactiveIcon: <Book size={iconSize} color={inactive} />,
     href: "/myrecipe",
+    id: "nav-icon-user-recipes",
   },
   {
     name: "Saved",
-    activeIcon: <Archive size={24} color="#000000" weight="fill" />,
-    inactiveIcon: <Archive size={24} color="#9E9E9E" />,
+    activeIcon: <Archive size={iconSize} color={active} weight="fill" />,
+    inactiveIcon: <Archive size={iconSize} color={inactive} />,
     href: "/saved",
+    id: "nav-icon-saved",
   },
 ];
