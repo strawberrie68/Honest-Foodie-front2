d097e1575a62c13916991594fe59bfa68ba633be
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-03-30 15:27:33 -0700

    responsive update

--- src/pages/navbar pages/saved/saved.jsx
+++ src/pages/navbar pages/saved/saved.jsx
@@ -1,36 +1,30 @@
 import NavBar from "../../../components/NavBar/NavBar";
 import { categoriesIcon } from "../../../shared/categoriesIcon";
 import { useSelector } from "react-redux";
-import { MagnifyingGlass, SlidersHorizontal } from "@phosphor-icons/react";
 
 import CategoryCard from "../../../components/CategoryCard";
 import ProfilePost from "../../../components/TypesOfRecipeCards/ProfilePost";
+import SearchBar from "../../../components/SearchBar";
 const Saved = () => {
   const user = useSelector((state) => state.auth.user);
   const savedRecipes = useSelector((state) => state.auth.user?.savedRecipes);
 
-  const ICON_SIZE = 18;
-  const ICON_BG_COLOR = "#A7A7A7";
-
   return (
-    <div className="flex">
-      <NavBar />
-      <div className="w-3/4 mt-10 flex p-4 h-full m-auto flex-col">
+    <div className="flex flex-col xs:flex-row h-screen w-full">
+      <div className="w-full xs:w-10 order-last xs:order-first fixed bottom-0 z-10">
+        <NavBar />
+      </div>
+      <div className="mt-8 m-auto xs:mt-10 flex h-full w-4/5 xs:w-3/4  xs:mr-12 md:ml-36 flex-col xs:p-4">
         <div className="flex justify-start flex-col w-full">
           <div className="flex flex-col xs:flex-row justify-between items-center">
-            <h1 className="mt-6 font-bold text-xl text-start">Saved recipes</h1>
-            <div className="bg-primary-gray-100 w-[250px] h-10 rounded-3xl px-4 items-center flex justify-between mt-4">
-              <div className="flex gap-4">
-                <MagnifyingGlass size={ICON_SIZE} />
-                <div className="text-xxs text-primary-gray-200">Search</div>
-              </div>
-              <div className="flex gap-2">
-                <div className="border-px border-l border-primary-gray-200 h-4 w-0"></div>
-                <SlidersHorizontal size={ICON_SIZE} color={ICON_BG_COLOR} />
-              </div>
+            <h1 className="mt-6 font-bold text-xl text-start my-4 mr-2">
+              Saved recipes
+            </h1>
+            <div>
+              <SearchBar />
             </div>
           </div>
-          <div className=" flex justify-start mt-8 gap-1 overflow-x-scroll">
+          <div className=" flex justify-start mt-8 gap-1 overflow-x-scroll  ">
             {categoriesIcon.map((category, i) => (
               <CategoryCard key={i} icon={category.icon} name={category.name} />
             ))}
@@ -49,7 +43,7 @@ const Saved = () => {
 
           <div className="mt-6 mb-20">
             <p className="font-semibold text-lg">To make</p>
-            {/* TODO: How to save a recipe to make later */}
+            {/* TODO: Save a recipe to make later */}
             {user && savedRecipes.length > 0 ? (
               savedRecipes?.map((recipe) => (
                 <div className="flex mt-2">
