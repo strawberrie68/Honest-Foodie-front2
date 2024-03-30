cfe7f830a572a21b9e06af7e6f39513ec9f71cd5
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-03-30 15:53:07 -0700

    chore: cleaned and refactored

--- src/pages/navbar pages/explore/explore.jsx
+++ src/pages/navbar pages/explore/explore.jsx
@@ -1,6 +1,5 @@
 import NavBar from "../../../components/NavBar/NavBar";
 import { categoriesIcon } from "../../../shared/categoriesIcon";
-import { MagnifyingGlass, SlidersHorizontal } from "@phosphor-icons/react";
 
 import CategoryCard from "../../../components/CategoryCard";
 import ProfilePost from "../../../components/TypesOfRecipeCards/ProfilePost";
@@ -8,16 +7,14 @@ import FeaturedRecipeCard from "../../../components/TypesOfRecipeCards/FeaturedR
 import { featuredCategories } from "../../../shared/featuredCategories";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
-const iconSize = 18;
-const iconBgColor = "#A7A7A7";
+import SearchBar from "../../../components/SearchBar";
 
 const Explore = () => {
   const [recipes, setRecipes] = useState(null);
 
   useEffect(() => {
     const getRecipe = async () => {
-      const result = await axios
+      await axios
         .get(`${process.env.REACT_APP_SERVER_URL}/api/recipe/`)
         .then((response) => {
           setRecipes(response.data);
@@ -27,32 +24,29 @@ const Explore = () => {
     getRecipe();
   }, []);
   return (
-    <div className="flex">
-      <NavBar />
-      <div className="m-auto mt-10 flex h-full w-3/4 flex-col p-4">
-        <div className="flex w-full flex-col justify-start">
-          <div className="flex flex-col items-center justify-between xs:flex-row">
-            <h1 className="mt-6 text-start text-xl font-bold">Explore</h1>
-            <div className="search mt-4 flex h-10 w-[250px] items-center justify-between rounded-3xl bg-primary-gray-100 px-4">
-              <div className="flex gap-4">
-                <MagnifyingGlass size={iconSize} />
-                <div className="text-xxs text-primary-gray-200">Search</div>
-              </div>
-              <div className="flex gap-2">
-                <div className="border-px h-4 w-0 border-l border-primary-gray-200"></div>
-                <SlidersHorizontal size={iconSize} color={iconBgColor} />
-              </div>
+    <div className="flex flex-col xs:flex-row h-screen w-full">
+      <div className="w-full xs:w-10 order-last xs:order-first fixed bottom-0 z-10">
+        <NavBar />
+      </div>
+      <div className="mt-8 m-auto xs:mt-10 flex h-full w-4/5 xs:w-3/4  xs:mr-12 md:ml-36 flex-col xs:p-4">
+        <div className="flex justify-start flex-col w-full">
+          <div className="flex flex-col xs:flex-row justify-between items-center">
+            <h1 className="mt-6 font-bold text-xl text-start my-4 mr-2">
+              Explore
+            </h1>
+            <div>
+              <SearchBar />
             </div>
           </div>
         </div>
-        <div className="mt-8 grid w-full grid-cols-3 xs:grid-cols-4 sm:flex">
+        <div className="mt-8 grid w-full grid-cols-4 xs:grid-cols-4 sm:flex">
           {categoriesIcon.map((category, i) => (
             <CategoryCard key={i} icon={category.icon} name={category.name} />
           ))}
         </div>
         <div className="mt-6 featured">
           <p className="ml-2 text-sm font-bold">Featured</p>
-          <div className="mt-2 flex gap-4">
+          <div className="mt-2 flex gap-4 overflow-scroll">
             {featuredCategories.map((category, i) => (
               <FeaturedRecipeCard key={i} category={category} />
             ))}
