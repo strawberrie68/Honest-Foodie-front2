dd42b4183e3475c108394a1d7201da66b1b9cddb
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-11-26 18:57:46 -0800

    fix: dashboard styling

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -12,7 +12,7 @@ import ProfilePost from "../../components/TypesOfRecipeCards/ProfilePost";
 const Dashboard = () => {
   const [recipes, setRecipes] = useState([]);
 
-    const getRecipes = async () => {
+  const getRecipes = async () => {
     try {
       const result = await axios.get("http://localhost:3003/api/recipe/");
       setRecipes(result.data || []);
@@ -23,31 +23,31 @@ const Dashboard = () => {
 
   useEffect(() => {
     getRecipes();
-  }, [])
+  }, []);
 
   console.log(recipes);
   return (
     <div className="flex">
       <NavBar />
-      <div className="w-full ml-24">
-        <div className="pt-4 m-auto mt-16 flex w-auto max-w-2xl flex-col items-center px-4 mb-0 pb-0">
+      <div className="w-full">
+        <div className="m-auto mb-0 mt-16 flex w-auto max-w-2xl flex-col items-center px-4 pb-0 pt-4">
           <div>
             <SearchBar />
           </div>
-          <div className="h-50 m-auto mt-10 mb-8 flex w-full rounded-3xl bg-black bg-hero-ramen p-4 text-white max-w-xl">
+          <div className="h-50 m-auto mb-8 mt-10 flex w-full max-w-xl rounded-3xl bg-black bg-hero-ramen p-4 text-white">
             <Link to="/explore">
               <div className="w-full p-2 sm:w-1/2">
                 <p className="text-2xl font-semibold">Trending Pages</p>
                 <p className="w-full text-xxs	font-thin xs:w-2/4 sm:w-full">
                   Checkout some of the most popular recipes that are trending.
                 </p>
-                <div className="mt-2 flex w-1/2 items-center justify-center rounded-3xl bg-white p-1 sm:mt-6 hover:bg-primary-blue-500 hover:text-white transition duration-100">
+                <div className="mt-2 flex w-1/2 items-center justify-center rounded-3xl bg-white p-1 transition duration-100 hover:bg-primary-blue-500 hover:text-white sm:mt-6">
                   <p className="text-sm font-semibold text-black">Explore</p>
                 </div>
               </div>
             </Link>
           </div>
-          <div className="grid grid-cols-4 gap-1 overflow-x-scroll sm:grid-cols-5 sm:flex sm:w-[500px] w-full ">
+          <div className="grid w-full grid-cols-4 gap-1 overflow-x-scroll sm:flex sm:w-[500px] sm:grid-cols-5 ">
             {categoriesIcon.map((category) => (
               <CategoryCard
                 key={category.id}
@@ -58,14 +58,12 @@ const Dashboard = () => {
           </div>
           <div className="flex w-full flex-col justify-start">
             <h1 className="mt-6 text-start text-xl font-bold">Popular</h1>
-            <div className="w-full mt-2 flex flex-wrap xs:grid  xs:grid-cols-3  gap-4 sm:gap-10">
+            <div className="mt-2 flex w-full flex-wrap gap-4  xs:grid  xs:grid-cols-3 sm:gap-10">
               {/* TODO filter so that - show popular recipes */}
               {recipes &&
                 recipes
                   .slice(0, 3)
-                  .map((recipe, id) => (
-                    <ProfilePost key={id} post={recipe} />
-                  ))}
+                  .map((recipe, id) => <ProfilePost key={id} post={recipe} />)}
             </div>
           </div>
           <div className="flex w-full flex-col justify-start">
