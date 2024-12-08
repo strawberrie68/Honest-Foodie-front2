a31479e82d84c4e932c733f8764d3d697bfb4a65
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-12-08 10:45:48 -0800

    add: Post Badge component

---
+++ src/components/PostBadge/PostBadge.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const PostBadge = ({ children, isReview = false, className = "" }) => {
+  return (
+    <div
+      className={`
+        absolute 
+        m-4 
+        rounded-3xl 
+        border 
+        border-white 
+        border-opacity-25 
+        px-4 
+        py-1
+        text-white
+        backdrop-blur-md
+        ${className}
+      `}
+      aria-label={isReview ? "Review content" : "Recipe content"}
+      role="tag"
+    >
+      <span className="text-xxs font-medium">{children}</span>
+    </div>
+  );
+};
+
+export default PostBadge;
