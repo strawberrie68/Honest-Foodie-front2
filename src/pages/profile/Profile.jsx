068d75c33a1ebc46dd0b87579408c2de2c77475b
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-12-08 17:39:24 -0800

    add: search to user profile page to filter for user recipes

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { categoriesIcon } from "../../shared/categoriesIcon";
+import { ProfileSection } from "../../shared/profileSection";
 
 import NavBar from "../../components/NavBar/NavBar";
+import CategoryCard from "../../components/CategoryCard";
 import ProfileHeader from "../../components/ProfileHeader";
 import UserCategoryNav from "../../components/UserCategoryNav";
 
@@ -8,13 +13,7 @@ import ProfileAllPosts from "./ProfileSections/ProfileAllPosts";
 import UserTastebuds from "./ProfileSections/UserTastebuds";
 import UserReviews from "./ProfileSections/UserReviews";
 import UserAllRecipes from "./ProfileSections/UserAllRecipes";
-
-import CategoryCard from "../../components/CategoryCard";
-import { categoriesIcon } from "../../shared/categoriesIcon";
-import { MagnifyingGlass, SlidersHorizontal } from "@phosphor-icons/react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import { ProfileSection } from "../../shared/profileSection";
+import SearchBar from "../../components/SearchBar";
 
 const Profile = () => {
   const apiUrl = import.meta.env.VITE_API_URL;
@@ -22,16 +21,22 @@ const Profile = () => {
   const { userId } = params;
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-
   const [selectedProfileSection, setSelectedProfileSection] = useState(
     ProfileSection.ALL,
   );
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit] = useState(10);
+  const [sortBy, setSortBy] = useState("createdAt");
+  const [sortOrder, setSortOrder] = useState("desc");
 
   const getUser = async (userId) => {
     try {
       setIsLoading(true);
       const response = await axios.get(`${apiUrl}/api/users/${userId}/public`);
-      console.log("API Response:", response.data);
       setUser(response.data);
     } catch (error) {
       console.error("Could not get user", error);
@@ -40,13 +45,69 @@ const Profile = () => {
     }
   };
 
+  const searchUser = async () => {
+    try {
+      setIsLoading(true);
+      const query = searchValue || selectedCategory || "";
+      const { data } = await axios.get(
+        `${apiUrl}/api/recipes/user/${userId}/search`,
+        {
+          params: {
+            q: query,
+            page,
+            limit,
+            sortBy,
+            sortOrder,
+            category: selectedCategory,
+          },
+        },
+      );
+      setSearchResults(data);
+    } catch (error) {
+      console.error("error searching users recipes", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  console.log("search result", searchResults);
+
+  const handleCategoryClick = (categoryName) => {
+    setSearchValue("");
+    setSelectedCategory(categoryName);
+    setIsSearching(true);
+    setPage(1); // Reset pagination when changing category
+  };
+
+  const handleSearchChange = (value) => {
+    setSearchValue(value);
+    setSelectedCategory(null);
+    setIsSearching(true);
+    setPage(1); // Reset pagination when searching
+  };
+
   useEffect(() => {
     if (userId) {
       getUser(userId);
     }
   }, [userId]);
 
-  if (isLoading) {
+  useEffect(() => {
+    if (isSearching) {
+      searchUser();
+    }
+  }, [searchValue, selectedCategory, page, sortBy, sortOrder]);
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const handleSort = (field) => {
+    setSortBy(field);
+    setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+  };
+
+  if (isLoading && !isSearching) {
     return <div>Loading</div>;
   }
 
@@ -72,46 +133,89 @@ const Profile = () => {
       <div className="w-full">
         <ProfileHeader user={user} />
         <div className="m-auto mt-2 flex w-full flex-col p-4 lg:w-4/5">
-          <div className="">
-            <UserCategoryNav
-              activeLabel={selectedProfileSection}
-              onNavItemClick={(section) => setSelectedProfileSection(section)}
-            />
-          </div>
+          <UserCategoryNav
+            activeLabel={selectedProfileSection}
+            onNavItemClick={(section) => {
+              setSelectedProfileSection(section);
+              setIsSearching(false);
+              setSearchValue("");
+              setSelectedCategory(null);
+            }}
+          />
 
           {/* Food category filter */}
           {selectedProfileSection !== ProfileSection.TASTE_ID && (
-            <div className="mb-8 mt-8 flex justify-start gap-1 overflow-x-scroll ">
-              {categoriesIcon.map((category, i) => (
-                <CategoryCard
-                  key={i}
-                  icon={category.icon}
-                  name={category.name}
-                />
+            <nav className="mb-8 mt-8 flex justify-start gap-1 overflow-x-scroll">
+              {categoriesIcon.map(({ name, icon }) => (
+                <button
+                  key={name}
+                  onClick={() => handleCategoryClick(name)}
+                  className={`CategoryCard ${
+                    selectedCategory === name ? "border-primary border-2" : ""
+                  }`}
+                  tabIndex="0"
+                >
+                  <CategoryCard icon={icon} name={name} />
+                </button>
               ))}
-            </div>
+            </nav>
           )}
 
           <div className="flex w-full flex-col justify-start">
-            <div className="flex items-center justify-between gap-4">
+            <div className="flex flex-col  justify-start gap-4 sm:flex-row sm:items-center sm:justify-between">
               <h1 className="profile-title mt-6 text-start text-xl font-bold">
                 {selectedProfileSection}
               </h1>
-              <div className="flex h-10 w-[250px] items-center justify-between rounded-3xl bg-primary-gray-100 px-4">
-                <div className="flex gap-4">
-                  <MagnifyingGlass size={18} />
-                  <div className="text-xxs text-primary-gray-200">Search</div>
-                </div>
-                <div className="flex gap-2">
-                  <div className="border-px h-4 w-0 border-l border-primary-gray-200"></div>
-                  <SlidersHorizontal size={18} color="#A7A7A7" />
+              <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+                <span className="text-neutral-500 sm:basis-24">Sort by</span>
+                <div className="flex items-center gap-4">
+                  <select
+                    className="rounded-full bg-neutral-100 px-3 py-2"
+                    value={sortBy}
+                    onChange={(e) => handleSort(e.target.value)}
+                  >
+                    <option className="text-sm" value="createdAt">
+                      Date
+                    </option>
+                    <option className="text-sm" value="title">
+                      Title
+                    </option>
+                  </select>
+                  <SearchBar onSearchChange={handleSearchChange} />
                 </div>
               </div>
             </div>
-            {/* Switch between user profile sections */}
-            <div className="mb-6 mt-2 flex gap-2">
-              {profileSectionComponents[selectedProfileSection]}
-            </div>
+
+            {/* Search Results or Default Content */}
+            {isSearching ? (
+              <div className="mb-6 mt-8">
+                {isLoading ? (
+                  <div>Searching...</div>
+                ) : (
+                  <>
+                    <h2 className="mb-4">Search Results</h2>
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                      {searchResults.recipes?.map((recipe) => (
+                        <UserAllRecipes key={recipe.id} recipes={[recipe]} />
+                      ))}
+                      {searchResults.recipes?.length === 0 && <p>No results</p>}
+                    </div>
+                    {searchResults.hasMore && (
+                      <button
+                        onClick={handleLoadMore}
+                        className="bg-primary mt-4 rounded-md px-4 py-2 text-white"
+                      >
+                        Load More
+                      </button>
+                    )}
+                  </>
+                )}
+              </div>
+            ) : (
+              <div className="mb-6 mt-8 flex gap-2">
+                {profileSectionComponents[selectedProfileSection]}
+              </div>
+            )}
           </div>
         </div>
       </div>
