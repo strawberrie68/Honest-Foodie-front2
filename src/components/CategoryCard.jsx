25d73e6959b50d8267a99376ba9cb5d566bbf096
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-12-11 15:16:15 -0800

    update: category card with semantic html

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,8 +1,8 @@
 const CategoryCard = ({ icon, name }) => {
   return (
     <div className="m-px flex flex-col items-center justify-center p-4">
-      <div className="flex h-[50px] w-[50px] items-center justify-center rounded-3xl bg-primary-gray-100">
-        {icon}
+      <div className="flex h-[50px] w-[50px] items-center justify-center rounded-3xl bg-primary-gray-100 text-2xl">
+        <span>{icon}</span>
       </div>
       <p className="mt-2 text-xxs font-semibold">{name}</p>
     </div>
