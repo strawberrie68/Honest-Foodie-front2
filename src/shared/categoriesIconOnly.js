267903e1ec29d65e3669d9b15a556054964aae85
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-12-13 11:39:47 -0800

    add: new category to categoryNav

---
+++ src/shared/categoriesIconOnly.js
@@ -0,0 +1,37 @@
+export const categoriesIconOnly = [
+  {
+    name: "Breaky",
+    icon: "🍳",
+    id: "categoryIcon-egg",
+  },
+  {
+    name: "Meat",
+    icon: "🍗",
+    id: "categoryIcon-meat",
+  },
+  {
+    name: "Desserts",
+    icon: "🎂",
+    id: "categoryIcon-desserts",
+  },
+  {
+    name: "Veggies",
+    icon: "🥗",
+    id: "categoryIcon-veggies",
+  },
+  {
+    name: "Bread",
+    icon: "🥐",
+    id: "categoryIcon-bread",
+  },
+  {
+    name: "Keto",
+    icon: "🥦",
+    id: "categoryIcon-keto",
+  },
+  {
+    name: "Drinks",
+    icon: "🧋",
+    id: "categoryIcon-keto",
+  },
+];
