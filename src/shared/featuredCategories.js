6534dbf133b5d9b8146e8cb4d8f7de0364c5ae50
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-12-13 11:40:48 -0800

    add: keyword to featureCategory nav

--- src/shared/featuredCategories.js
+++ src/shared/featuredCategories.js
@@ -1,18 +1,22 @@
 export const featuredCategories = [
   {
     picturePath: "./src/assets/featuredCategories/egg.jpg",
-    title: "Top Recipes",
+    title: "Top Breakfast",
+    keyword: "breakfast",
   },
   {
     picturePath: "./src/assets/featuredCategories/toast.jpg",
-    title: "Top Trending",
+    title: "Top Toasts",
+    keyword: "Toast",
   },
   {
     picturePath: "./src/assets/featuredCategories/breakfast.jpg",
-    title: "Popular",
+    title: "Top Salads",
+    keyword: "salad",
   },
   {
     picturePath: "./src/assets/featuredCategories/drink.jpg",
-    title: "Celebration",
+    title: "Drinks",
+    keyword: "Drink",
   },
 ];
